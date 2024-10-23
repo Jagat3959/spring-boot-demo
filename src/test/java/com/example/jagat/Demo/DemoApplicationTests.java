93a108b760cc83905009e7346de586c030644026
Jagatananda_choudhury <jagatananda.choudhury@capgemini.com>
2024-10-23 10:32:03 +0530

    Initial commit

---
+++ src/test/java/com/example/jagat/Demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jagat.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
